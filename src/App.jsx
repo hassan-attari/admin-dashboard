36ceae7a9f8fbbbd390746d980669045cc668fe8
Abbas Sepahvand <abbassep@outlook.com>
2023-01-10 14:26:42 +0330

    commite

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Login from './features/Identity/components/login/login';
+import Register from './features/Identity/components/register/register';
 
 const App = () => {
   return (
-<p> پلتفرم آموزش آنلاین</p>
+ <Login/>
   )
 };
 
---
+++ src/readme.md
@@ -0,0 +1,3 @@
+1- ui for register
+2- ui for login
+3- alias for @assets
