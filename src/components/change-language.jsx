3484ce30cc4b2654a0fe077af2ef9e6ef5dd314d
Abbas Sepahvand <abbassep@outlook.com>
2023-01-31 16:11:14 +0330

    update

---
+++ src/components/change-language.jsx
@@ -0,0 +1,46 @@
+import usFlag from "@assets/images/us.png";
+import faFlag from "@assets/images/fa.png";
+import { useState } from "react";
+import { useRef } from "react";
+import { useEffect } from "react";
+const ChangeLanguage = () => {
+  const [show, setShow] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+    const checkIfclickOutside = (e) => {
+      if (show && ref.current && !ref.current.contains(e.target)) {
+        setShow(false);
+      }
+    };
+
+    document.addEventListener("mousedown", checkIfclickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", checkIfclickOutside);
+    };
+  }, [show]);
+
+  return (
+    <div className="dropdown">
+      <a
+        className="nav-flag dropdown-toggle"
+        onClick={() => setShow(true)}
+      >
+        <img src={usFlag} alt="English" />
+      </a>
+      <div ref={ref} className={`dropdown-menu dropdown-menu-end ${show ? 'show' : undefined}`}>
+        <a className="dropdown-item fw-bolder" style={{ textAlign: "right" }}>
+          <img src={faFlag} width="20" className="ms-2" />
+          <span className="align-middle ">فارسی</span>
+        </a>
+        <a className="dropdown-item fw-bolder" style={{ textAlign: "right" }}>
+          <img src={usFlag} width="20" className="ms-2" />
+          <span className="align-middle">English</span>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default ChangeLanguage;
--- src/layouts/identity-layout.jsx
+++ src/layouts/identity-layout.jsx
@@ -1,39 +1,13 @@
 import { Outlet } from "react-router-dom";
-import usFlag from "@assets/images/us.png";
-import faFlag from "@assets/images/fa.png";
+import ChangeLanguage from "../components/change-language";
+
 
 const IdentityLayout = () => {
   return (
     <>
       <div className="main d-flex justify-content-center w-100">
         <nav className="navbar shadow-sm">
-          <div className="dropdown">
-            <a
-              className="nav-flag dropdown-toggle"
-            >
-              <img src={usFlag} alt="English" />
-            </a>
-            <div
-              className="dropdown-menu dropdown-menu-end  show"
-            >
-              <a className="dropdown-item fw-bolder" style={{textAlign: 'right'}}>
-                <img
-                  src={faFlag}
-                  width="20"
-                  className="ms-2"
-                />
-                <span className="align-middle ">فارسی</span>
-              </a>
-              <a className="dropdown-item fw-bolder" style={{textAlign: 'right'}}>
-                <img
-                  src={usFlag}
-                  width="20"
-                  className="ms-2"
-                />
-                <span className="align-middle">English</span>
-              </a>
-            </div>
-          </div>
+         <ChangeLanguage/>
         </nav>
         <main className="content d-flex p-0">
           <div
