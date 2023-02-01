f2bcf558a23ef15a6f1bea6a3e4a85aa68dada86
Abbas Sepahvand <abbassep@outlook.com>
2023-02-01 16:33:31 +0330

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,22 @@ import React, { Suspense } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
 import './core/i18n';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useAppContext } from './contexts/app/app-context';
 
 const App = () => {
+  const {theme} = useAppContext();
+  useEffect(() => {
+    const head = document.head;
+    const link = document.createElement('link');
+    link.rel="stylesheet";
+    link.href= `css/${theme}.css`;
+    head.appendChild(link);
+
+    return () => {head.removeChild(link)}
+  }, [theme]);
+
   return <RouterProvider router={router}/>
 };
 
--- src/contexts/app/app-context.jsx
+++ src/contexts/app/app-context.jsx
@@ -6,7 +6,8 @@ import appReducer from "./app-reducer";
 
 const AppContext = createContext();
 const initialState = {
-    language: localStorage.getItem('language') || 'fa'
+    language: localStorage.getItem('language') || 'fa',
+    theme: localStorage.getItem('theme') || 'light'
 };
 
 const AppProvider = ({children}) => {
@@ -17,12 +18,20 @@ const AppProvider = ({children}) => {
         dispatch({type: 'CHANGE_LANGUAGE', payload: language});
     }
 
+    const changeTheme = (theme) => {
+        dispatch({type: 'CHANGE_THEME', payload: theme});
+    }
+
     useEffect(() => {
         i18n.changeLanguage(state.language);
         localStorage.setItem('language', state.language);
     }, [state.language])
 
-    return <AppContext.Provider value={{...state, changeLanguage}}>
+    useEffect(() => {
+        localStorage.setItem('theme', state.theme);
+    }, [state.theme])
+
+    return <AppContext.Provider value={{...state, changeLanguage, changeTheme}}>
         {children}
     </AppContext.Provider>
 }
